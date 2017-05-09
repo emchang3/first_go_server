6788185aea8786feb76f4553c79ac7f1e7ec25c2
Ezra M. Chang <ezra.chang@3dresults.com>
2017-05-09 13:51:36 -0500

    Server equipped to gzip. Changed UI shadows.

--- go_server.go
+++ go_server.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-  "fmt"
-  "github.com/joho/godotenv"
-  "log"
-  "net/http"
-  // "net/url"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/NYTimes/gziphandler"
+	"github.com/joho/godotenv"
+	// "net/url"
 )
 
 func routeHandler() {
-  fs := http.FileServer(http.Dir("public"))
-  http.Handle("/public/", http.StripPrefix("/public/", fs))
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
 
-  http.HandleFunc("/", index)
-  http.HandleFunc("/post/", contentPost)
-  http.HandleFunc("/submit", receiveContent)
+	indexGz := gziphandler.GzipHandler(http.HandlerFunc(index))
+	postGz := gziphandler.GzipHandler(http.HandlerFunc(contentPost))
+
+	http.Handle("/", indexGz)
+	http.Handle("/post/", postGz)
+	http.HandleFunc("/submit", receiveContent)
 }
 
 func main() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  routeHandler()
+	routeHandler()
 
-  port := getPort()
-  fmt.Printf("\n--- Listening on: %v\n\n", port)
+	port := getPort()
+	fmt.Printf("\n--- Listening on: %v\n\n", port)
 
-  log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
--- src/base.ts
+++ src/base.ts
@@ -12,15 +12,15 @@
       bottom.className = 'menu-bottom-bar menu-bar-flat';
       middle.className = 'menu-middle-bar';
       top.className = 'menu-top-bar menu-bar-flat';
-      nav.className = 'nav-menu-closed mdl-shadow--2dp';
-      header.className = 'header-original mdl-shadow--2dp';
+      nav.className = 'nav-menu-closed mdl-shadow--16dp';
+      header.className = 'header-original mdl-shadow--4dp';
       menuOpen = false;
     } else {
       bottom.className = 'menu-bottom-bar menu-bottom-tilt';
       middle.className = 'menu-middle-fade';
       top.className = 'menu-top-bar menu-top-tilt';
-      nav.className = 'nav-menu-open mdl-shadow--2dp';
-      header.className = 'header-shifted mdl-shadow--2dp';
+      nav.className = 'nav-menu-open mdl-shadow--16dp';
+      header.className = 'header-shifted mdl-shadow--4dp';
       menuOpen = true;
     }
   }
